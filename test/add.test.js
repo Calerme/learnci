858a5f38288b1bc03eea6fa7980d5728b3def575
Caler Loo <g-i@live.cn>
2018-10-09 17:04:31 +0800

    '11'

--- test/add.test.js
+++ test/add.test.js
@@ -1,6 +1,7 @@
 const add = require('../src/add')
+const assert = require('assert')
 describe('Math', function () {
 	it('1 plus 1 shuold return 2', function () {
-		throw new Error('Just a test')	
+		assert(add(1,1), 2)
 	})
 })
